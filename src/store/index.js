725b44f859027a03aa2eab80bab9cb7a56fa9ccc
Murilo Zelic <murilozelic@gmail.com>
2020-03-01 21:05:01 -0300

    Header e notificacoes configurados

---
+++ src/store/index.js
@@ -0,0 +1,24 @@
+import { persistStore } from 'redux-persist';
+import createSagaMiddleware from 'redux-saga';
+
+import createStore from './createStore';
+import persistReducer from './persistReducers';
+
+import rootReducer from './modules/rootReducer';
+import rootSaga from './modules/rootSaga';
+
+const sagaMonitor =
+  process.env.NODE_ENV === 'development'
+    ? console.tron.createSagaMonitor()
+    : null;
+
+const SagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+const middlewares = [SagaMiddleware];
+
+const store = createStore(persistReducer(rootReducer), middlewares);
+const persistor = persistStore(store);
+
+SagaMiddleware.run(rootSaga);
+
+export { store, persistor };
