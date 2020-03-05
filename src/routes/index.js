77176c920fc6290f6202c563e5ebd608a15c7a66
murilozelic <murilozelic@gmail.com>
2020-03-04 22:37:00 -0300

    Funcionalidades Perfil

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,8 +15,8 @@ export default function Routes() {
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
 
-      <Route path="/dashboard" component={Dashboard} isPrivate />
-      <Route path="/profile" component={Profile} isPrivate />
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
     </Switch>
   );
 }
--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -26,6 +26,12 @@ export default function auth(state = INITIAL_STATE, action) {
         break;
       }
 
+      case '@auth/signOut': {
+        draft.token = null;
+        draft.signed = false;
+        break;
+      }
+
       default:
     }
   });
